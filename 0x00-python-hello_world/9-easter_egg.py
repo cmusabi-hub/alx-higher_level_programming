a956b712c28933d4cddfc29bcc07c12e05522006
Charles Musabi <musabicharles91@gmail.com>
2023-03-14 13:03:47 +0300

    The Zen of Python

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 The_Zen_of_Python = """
+    The Zen of Python, by Tim Peters
+
     Beautiful is better than ugly.
     Explicit is better than implicit.
     Simple is better than complex.
