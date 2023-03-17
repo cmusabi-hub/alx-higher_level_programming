13648e4f879a61428927446114daaadaff003b09
Charles Musabi <musabicharles91@gmail.com>
2023-03-17 11:52:52 +0300

    Prints posible two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,11 @@
-#!/
+#!/usr/bin/python3
+
+for i in range(10):
+    for j in range(10):
+        if i != j and i < 9:
+            str_int = str(i) + str(j)
+            if int(str_int) < 89:
+                print(f"{str_int}", end=", ")
+            else:
+                print(f"{str_int}")
+
