4fb6bbc540d5019698fbbb40c84dded6716eb490
Charles Musabi <musabicharles91@gmail.com>
2023-03-27 21:50:54 +0300

    Add module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-import add_0.py
-a = 1
-b = 2
-print(f"{a} + {b} = {add(a, b)}")
+
+if __name__ = "__main__":
+    import add_0.py
+    a = 1
+    b = 2
+    print(f"{a} + {b} = {add(a, b)}")
