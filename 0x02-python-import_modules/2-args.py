c2d0d1d1e1ffdf0d2f4037d214dc48ee4c74d61a
Charles Musabi <musabicharles91@gmail.com>
2023-03-29 11:01:41 +0300

    Program that prints the arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ from sys import argv
 def main():
     argument = len(argv) - 1
     if argument == 0:
-           print("0 arguments.")
+        print("0 arguments.")
     elif argument == 1:
         print("1 argument:")
         print("{}: {}".format(argument, argv[argument]))
@@ -13,5 +13,7 @@ def main():
         print("{} arguments:".format(argument))
         for i in range(1, argument + 1):
             print("{}: {}".format(i, argv[i]))
+
+
 if __name__ == "__main__":
     main()
