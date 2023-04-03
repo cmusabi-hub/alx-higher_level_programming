a1803d25ec83d3109091d9d57b04d7da3aa229ce
Charles Musabi <musabicharles91@gmail.com>
2023-04-03 17:28:58 +0300

    Function that  an element from a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
+
 def element_at(my_list, idx):
-    for i in my_list:
-        if idx < 0 | idx > len(mylist) - 1:
-            return None
-        return(my_list[idx])
+    if idx < 0 | idx > len(mylist) - 1:
+        return None
+    return(my_list[idx])
