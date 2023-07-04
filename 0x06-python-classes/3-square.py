b0d48aa0c51826ccb150bf8699e68bb8759898fb
Charles Musabi <musabicharles91@gmail.com>
2023-07-04 17:22:48 +0300

    Class Square with Private instance size that gets its area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,9 +6,9 @@ class Square:
     def __init__(self, size=0):
         """Initialization of variable size"""
         if not isinstance(size, int):
-            TypeError("size must be an integer")
+            raise TypeError("size must be an integer")
         elif size < 0:
-            ValueError("size must be >= 0")
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
 
