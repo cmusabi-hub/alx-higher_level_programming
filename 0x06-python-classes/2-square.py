d15e219ec9ebb86c5afde888feebdaab220152fd
Charles Musabi <musabicharles91@gmail.com>
2023-07-04 15:34:18 +0300

    Class Square with Private instance attribute size that checks exceptions

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Definition of a class Square"""
+
+
+class Square:
+    """ Initialization of variables"""
+    def __init__(self, size = 0):
+        """ Initialization of variable size
+        the size should be integer type.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
