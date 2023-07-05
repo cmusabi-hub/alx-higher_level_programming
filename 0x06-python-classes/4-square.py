05fca67122a062eccedc223383c0bf837f1f2898
Charles Musabi <musabicharles91@gmail.com>
2023-07-05 12:14:44 +0300

    Class Square created using properties and setters

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Definition of a Class"""
+
+
+class Square:
+    """Creating square class"""
+    def __init__(self, size=0):
+        self.size = size
+    @property
+    def size(self):
+    """Defining the property size"""
+        return self.__size
+    @size.setter
+    def size(self, size):
+    """Checking the property size for errors"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    def area(self):
+    """Getting the area of the square"""
+        return (self.size * self.size)
