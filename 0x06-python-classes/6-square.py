dc5c2239a3d6d92797dc3e7fc510ceb27fcb6cc0
Charles Musabi <musabicharles91@gmail.com>
2023-07-06 13:17:44 +0300

    Class Square created using properties and setters and prints a square with # sign adding spaces with position private instance

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -48,5 +48,7 @@ class Square:
         if (self.__size == 0):
             print()
         else:
+            for _ in range(self.__position[1]):
+                print()
             for point in range(self.__size):
                 print(" " * self.__position[0] + "#" * self.__size)
