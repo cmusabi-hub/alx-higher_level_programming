92f8a05f26df2b6cc2f0dd5efc923aecc77c6373
Charles Musabi <musabicharles91@gmail.com>
2023-07-23 16:50:14 +0300

    Function that prints the first and last name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module with a say_my_name function"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Function that prints the name passed"""
+
+    if not (isinstance(first_name, str)):
+        raise TypeError("first_name must be a string")
+
+    if not (isinstance(last_name, str)):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
