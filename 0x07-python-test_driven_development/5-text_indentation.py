44eaef0fbc196a0c4e95ae5d4d0fc431e473daef
Charles Musabi <musabicharles91@gmail.com>
2023-07-26 10:27:11 +0300

    Function that prints a text with 2 new lines after each of these characters: ., ? and :

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -12,14 +12,15 @@ def text_indentation(text):
         raise TypeError("text must be a string")
     for index in range(len(text)):
         new_char += text[index]
+
         if text[index] == "\n":
             final_char += new_char + '\n'
         elif text[index] in char_s:
             new_char = new_char.strip()
             final_char += new_char + "\n\n"
             new_char = ""
+
     new_char = new_char.strip()
-    if new_char:
-        final_char += new_char
+    final_char += new_char
 
     print(final_char)
