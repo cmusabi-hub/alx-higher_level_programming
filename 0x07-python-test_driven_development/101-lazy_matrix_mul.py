7d5052ef07d1570ec58f680c3e91059e442dd1e1
Charles Musabi <musabicharles91@gmail.com>
2023-07-28 12:05:11 +0300

    Module of matrix multiplication using numpy library

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import numpy as np
+"""Module for multiplication of matrices"""
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    m_a is first matrix
+    m_b is the secon matrix
+    """
+    matri_mult = np.dot(m_a, m_b)
+
+    return (matri_mult)
