76ac425be3b715140fcbfa7c80eef2fd75bb2e92
Charles Musabi <musabicharles91@gmail.com>
2023-09-10 13:33:32 +0300

    Program that prints possibel combination of two numbers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,5 @@
 for num_1 in range(0, 9):
     for num_2 in range(1, 10):
         if num_2 > num_1:
-            print("{:d}{:d}".format(num_1, num_2), end=", " if num_1 < 8 else "\n")
+            print("{:d}{:d}".format(num_1, num_2),
+                  end=", " if num_1 < 8 else "\n")
